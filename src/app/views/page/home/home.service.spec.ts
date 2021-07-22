ea5de0f44de83654e00642310dda9b0dd9ced3b2
Nguyen Le Vien <viennl@domain.vnext.vn>
2021-07-22 15:44:51 +0700

    create unit test home

--- src/app/views/page/home/home.service.spec.ts
+++ src/app/views/page/home/home.service.spec.ts
@@ -1,9 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
 
 import { HomeService } from './home.service';
 
 describe('HomeService', () => {
   let service: HomeService;
+  let comp: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +15,19 @@ describe('HomeService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be open dropdown', () => {
+    expect(comp.isOrderOpen).toBe(false, 'off at first');
+    comp.toogleOrderDropdown();
+    expect(comp.isOrderOpen).toBe(true, 'on after click');
+    comp.toogleOrderDropdown();
+    expect(comp.isOrderOpen).toBe(false, 'off after second click');
+  });
+
+  it('format date should be created', () => {
+    expect(comp.formatTimeBlog('')).toEqual('');
+    expect(comp.formatTimeBlog('2021-09-09 13:03:23')).toEqual('2021-09-09');
+    expect(comp.formatTimeBlog('2021/07/09 12:33:56')).toEqual('2021/07/09');
   });
 });
